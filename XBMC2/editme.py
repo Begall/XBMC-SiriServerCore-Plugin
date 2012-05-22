6d9f9354e80a42926481c21a23a1fee91ca0c020
Ben Galey <nightviper@gmail.com>
2012-05-22 23:23:59 +0100

    Initial login, play functions

---
+++ XBMC2/editme.py
@@ -0,0 +1,7 @@
+#from plugin import *
+
+
+def GetLogin(host='192.168.0.1', port='8082', username='Begall', password='password', mac_address=None): 
+        x = [host, port, username, password, mac_address]
+        return x 
+        

25fdf836b5a5377d7b9e4cd9702db91564ef7ed6
Ben Galey <nightviper@gmail.com>
2012-05-22 23:25:38 +0100

    Initial login, play functions#2

--- XBMC2/__init__.py
+++ XBMC2/__init__.py
@@ -1,1 +1,28 @@
-import json
+#from plugin import *
+from editme import GetLogin 
+
+try:
+    import jsonrpclib 
+except ImportError:
+    raise NecessaryModuleNotFound('XBMC plugin will not work: JSONRPCLIB not installed. To install, run "easy_install jsonrpclib"')
+
+def get_url():
+    if GetLogin()[0] and GetLogin()[1] and GetLogin()[2] and GetLogin()[3] != None: 
+       return 'http://%s:%s@%s:%s' %(GetLogin()[2], GetLogin()[3], GetLogin()[0], GetLogin()[1]) 
+    else: 
+       return '' 
+
+def play(self, json, item, playerid):
+    json.Playlist.Clear(playlistid=playerid)
+    json.Playlist.Add(playlistid=playerid, item=item)
+    json.Player.Open({ 'playlistid' : playerid})
+
+class XBMC2():
+
+      global json
+      try:
+         json = jsonrpclib.Server('%s/jsonrpc' %(get_url()))
+      except IOError: 
+         print "ERROR: Incomplete XBMC login information, please edit ~/editme.py" 
+
+

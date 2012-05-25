e2fb2727d4240b987c8579993a0acc60edd62736
Ben Galey <nightviper@gmail.com>
2012-05-25 19:11:22 +0100

    Basic utility & player control functions, both audio and video

--- XBMC2/__init__.py
+++ XBMC2/__init__.py
@@ -12,7 +12,7 @@ def get_url():
     else: 
        return '' 
 
-def play(self, json, item, playerid):
+def play(json, item, playerid):
     json.Playlist.Clear(playlistid=playerid)
     json.Playlist.Add(playlistid=playerid, item=item)
     json.Player.Open({ 'playlistid' : playerid})
@@ -24,5 +24,56 @@ class XBMC2():
          json = jsonrpclib.Server('%s/jsonrpc' %(get_url()))
       except IOError: 
          print "ERROR: Incomplete XBMC login information, please edit ~/editme.py" 
+
+
+      # Utility Functions
+
+
+      @register("en-US", "xbmc help") 
+      def help(self, speech, langauge, matchedRegex):
+          self.say("Placeholder", "Here is a list of available commands...") 
+          self.complete_request()
+
+      @register("en-US", ".*update (?P<librarytype>[\w]+) library.*")
+      def update_command(self, speech, langauge,  matchedRegex):
+         librarytype = string.capwords(matchedRegex.group('librarytype'))
+         if librarytype == 'Video':
+            json.VideoLibrary.Scan()
+         elif librarytype == 'Music':
+            json.AudioLibrary.Scan()
+         self.say("%s library updated" %(librarytype))
+         self.complete_request()
+      
+      @register("en-US", ".*clean (?P<librarytype>[\w]+) library.*")
+      def clean_command(self, speech, langauge, matchedRegex):
+          librarytype = string.capwords(matchedRegex.group('librarytype'))
+          if librarytype == 'Video':
+             json.VideoLibrary.Clean()
+          elif librarytype == 'Music':
+             json.AudioLibray.Scan()
+          self.say("%s library cleaned" %(librarytype)) 
+          self.complete_request()
+
+     # Player Controls
+             
+      @register("en-US", ".*stop (?P<librarytype>[\w]+) player.*")
+      def stop_command(self, speech, langauge, matchedRegex):
+          librarytype = string.capwords(matchedRegex.group('librarytype'))
+          if librarytype == 'Video': 
+             json.Player.Stop(playerid=1)
+          elif librarytype == 'Music':
+             json.Player.Stop(playerid=0)
+          self.say("%s player stopped" %(librarytype))
+          self.complete_request()  
+ 
+      @register("en-US", ".*(pause|paws|place|holes|polls|kohls|kohl's|post|resume) (?P<librarytype>[\w]+ player.*")
+      def pause_command(self, speech, langauge, matchedRegex):
+          librarytype = string.capwords(matchedRegex.group('librarype'))
+          if librarytype == 'Video': 
+             json.Player.PausePlay(playerid=1)
+          elif librarytype == 'Music':
+             json.Player.PausePlay(playerid=0)
+          self.say(" ", "%s player paused/resumed" %(librarytype))
+          self.complete_request()    
 
 
